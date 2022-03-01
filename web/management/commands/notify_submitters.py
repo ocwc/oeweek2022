9d849ed5818af179e312bd62c3470ede7b7a8ec3
Jan Gondol <jan@jangondol.com>
2022-03-01 12:35:53 +0100

    Update email notification code

--- web/management/commands/notify_submitters.py
+++ web/management/commands/notify_submitters.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand
-from django.core.mail import send_mail
+from mail_templated import send_mail
 from django.template.loader import get_template
 
 from web.models import Resource
@@ -9,23 +9,37 @@ class Command(BaseCommand):
     help = "Notifies submitters that they got accepted"
 
     def handle(self, *args, **options):
-        plaintext = get_template('emails/accepted.txt')
 
-        for resource in Resource.objects.filter(post_status='publish',
-                                                created__year=2009,
-                                                notified=False).exclude(email='')[:15]:
-            if Resource.objects.filter(email=resource.email, notified=True).count():
-                print('Skipping {} about #{}'.format(resource.email, resource.id))
-
-                resource.notified = True
-                resource.save()
-
+        for resource in Resource.objects.filter(
+                year=2022,
+                # created__year=2022,
+                post_status='publish',
+                # notified=False,
+                # email='example@example.org',
+        ):[:1]:#.exclude(email='')
+            print(resource)
+            if resource.notified==True:
+                print(f'Skipping {resource.email} about #{resource.id} (already notified)')
                 continue
 
-            resource.notified = True
-            resource.save()
+            print('sending to:', resource.email )
+            context = {
+                'year': resource.year,
+                'slug': resource.slug,
+                'title': resource.title,
+                'firstname': resource.firstname,
+            }
 
-            send_mail('Your Open Education Week submission has been approved', plaintext.render({}),
-                      'info@openeducationweek.org', [resource.email], fail_silently=False)
-
-            print('Emailed {} about #{}'.format(resource.email, resource.id))
+            try:
+                send_mail(
+                    "emails/accepted.tpl",
+                    context, # {}
+                    "info@openeducationweek.org",
+                    [resource.email],
+                    cc=['openeducationweek@oeglobal.org'],
+                )
+                resource.notified = True
+                resource.save()
+                print(f'Emailed {resource.email} about #{resource.id}')
+            except:
+                print(f'Failed to email {resource.email} about #{resource.id}')
