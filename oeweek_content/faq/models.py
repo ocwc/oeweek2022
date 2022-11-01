8e258f93c69edac24bccc937dd8d3ddb394b3657
Peter Hanecak <hanecak@hany.sk>
2022-11-01 13:55:50 +0100

    initial FAQ implementation

--- oeweek_content/faq/models.py
+++ oeweek_content/faq/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+from wagtail.models import Page
+from wagtail.fields import RichTextField
+from wagtail.admin.panels import FieldPanel
+from wagtail.search import index
+
+
+class FaqIndexPage(Page):
+    """
+    FAQ index page
+    """
+
+    intro = RichTextField(max_length=1000, help_text="FAQ index page intro")
+
+    content_panels = Page.content_panels + [FieldPanel("intro")]
+
+
+class FaqPage(Page):
+    """
+    FAQ page
+
+    We do not intend to have actual "one question, one answer" since we want just one single page,
+    but for now this seems to be the way.
+    """
+
+    # page title serves as question
+    answer = RichTextField(max_length=1000, help_text="Answer")
+    # TODO: ordering, hence some number, ...?
+
+    search_fields = Page.search_fields + [
+        index.SearchField("answer"),
+    ]
+
+    content_panels = Page.content_panels + [
+        FieldPanel("answer"),
+    ]
