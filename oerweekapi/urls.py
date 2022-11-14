855ba8f161cbba7a277bfbfb92069e57a56dd933
Peter Hanecak <hanecak@hany.sk>
2022-11-14 22:44:15 +0100

    WagTail based 'About' content linked to Django based content (+ black formatter kicking-in)

--- oerweekapi/urls.py
+++ oerweekapi/urls.py
@@ -10,7 +10,7 @@ from wagtail import urls as wagtail_urls
 from wagtail.documents import urls as wagtaildocs_urls
 
 # TODO: not sure about this one
-#from search import views as search_views
+# from search import views as search_views
 
 from web import views
 
@@ -40,63 +40,75 @@ router.register(r"event", EventViewSet, basename="Event")
 router.register(r"submission", SubmissionViewSet, basename="Submission")
 router.register(r"email-templates", EmailTemplateView)
 
-urlpatterns = [
-    url(r"^api/", include(router.urls)),
-    # url(r'^api/submission/', SubmissionView.as_view()),
-    url(r"^api/events-summary/", EventSummaryView.as_view()),
-    url(r"^api/twitter/", TwitterSearchResults.as_view()),
-    url(r"^api/request-access/", RequestAccessView.as_view()),
-    url(r"^admin/", admin.site.urls),
-    url(r"^api-auth/", include("rest_framework.urls", namespace="rest_framework")),
-    url(r"^api-token-auth/", rest_framework_jwt.views.obtain_jwt_token),
-    url(r"^api-token-refresh/", rest_framework_jwt.views.refresh_jwt_token),
-    url(r"^export/resources/$", ExportResources.as_view(), name="resource_export"),
-
-    url(r'^$', views.index, name='web_index'),
-    # url(r'^$', RedirectView.as_view(url='https://www.oeglobal.org/activities/open-education-week/', permanent=False), name='root_redirect'),
-
-    # url(r'^page/what-is-open-education-week/$', views.page__what_is_open_education_week),
-    # url(r'^page/faq/$', views.page__faq),
-    # url(r'^page/contribute/$', views.page__contribute),
-    url(r'^submit$', RedirectView.as_view(url='/contribute/', permanent=False)),
-    url(r'^submit/$', RedirectView.as_view(url='/contribute/', permanent=False)),
-    url(r'^submit-activity$', RedirectView.as_view(url='/contribute-activity/', permanent=False)),
-    url(r'^submit-activity/$', RedirectView.as_view(url='/contribute-activity/', permanent=False)),
-    url(r'^submit-asset$', RedirectView.as_view(url='/contribute-asset/', permanent=False)),
-    url(r'^submit-asset/$', RedirectView.as_view(url='/contribute-asset/', permanent=False)),
-
-    url(r'^contribute$', RedirectView.as_view(url='/contribute/', permanent=False)),
-    url(r'^contribute-activity$', RedirectView.as_view(url='/contribute-activity/', permanent=False)),
-    url(r'^contribute-asset$', RedirectView.as_view(url='/contribute-asset/', permanent=False)),
-    # 2022-04-05 -- replaced contribute links with redirects
-    # url(r'^contribute/$', views.contribute),
-    # url(r'^contribute-activity/$', views.contribute_activity),
-    # url(r'^contribute-asset/$', views.contribute_asset),
-    url(r'^contribute/$', RedirectView.as_view(url='/', permanent=False)),
-    url(r'^contribute-activity/$', RedirectView.as_view(url='/', permanent=False)),
-    url(r'^contribute-asset/$', RedirectView.as_view(url='/', permanent=False)),
-
-    path('edit/<uuid:identifier>/', views.edit_resource),
-    # OLD: url(r'^edit/$', views.edit_resource),
-    # url(r'^page/materials/$', views.page__materials),
-    url(r'^thanks/$', views.thanks),
-
-    url(r'^schedule$', RedirectView.as_view(url='/events/', permanent=False)),
-    url(r'^schedule/$', RedirectView.as_view(url='/events/', permanent=False)),
-    url(r'^events$', RedirectView.as_view(url='/events/', permanent=False)),
-    url(r'^events/$', views.show_events),
-    url(r'^resources$', RedirectView.as_view(url='/resources/', permanent=False)),
-    url(r'^resources/$', views.show_resources),
-    path('events/<int:year>/<str:slug>/', views.show_event_detail),
-    path('resources/<int:year>/<str:slug>/', views.show_resource_detail),
-
-    # url(r'^page/home/$', views.index, name='web_index'),
-
-    path("cms/", include(wagtailadmin_urls)),
-    path("documents/", include(wagtaildocs_urls)),
-    path('pages/', include(wagtail_urls)),
-    #path("search/", search_views.search, name="search"),
-] \
-+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
-+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
+urlpatterns = (
+    [
+        url(r"^api/", include(router.urls)),
+        # url(r'^api/submission/', SubmissionView.as_view()),
+        url(r"^api/events-summary/", EventSummaryView.as_view()),
+        url(r"^api/twitter/", TwitterSearchResults.as_view()),
+        url(r"^api/request-access/", RequestAccessView.as_view()),
+        url(r"^admin/", admin.site.urls),
+        url(r"^api-auth/", include("rest_framework.urls", namespace="rest_framework")),
+        url(r"^api-token-auth/", rest_framework_jwt.views.obtain_jwt_token),
+        url(r"^api-token-refresh/", rest_framework_jwt.views.refresh_jwt_token),
+        url(r"^export/resources/$", ExportResources.as_view(), name="resource_export"),
+        url(r"^$", views.index, name="web_index"),
+        # url(r'^$', RedirectView.as_view(url='https://www.oeglobal.org/activities/open-education-week/', permanent=False), name='root_redirect'),
+        # url(r'^page/what-is-open-education-week/$', views.page__what_is_open_education_week),
+        # url(r'^page/faq/$', views.page__faq),
+        # url(r'^page/contribute/$', views.page__contribute),
+        url(r"^submit$", RedirectView.as_view(url="/contribute/", permanent=False)),
+        url(r"^submit/$", RedirectView.as_view(url="/contribute/", permanent=False)),
+        url(
+            r"^submit-activity$",
+            RedirectView.as_view(url="/contribute-activity/", permanent=False),
+        ),
+        url(
+            r"^submit-activity/$",
+            RedirectView.as_view(url="/contribute-activity/", permanent=False),
+        ),
+        url(
+            r"^submit-asset$",
+            RedirectView.as_view(url="/contribute-asset/", permanent=False),
+        ),
+        url(
+            r"^submit-asset/$",
+            RedirectView.as_view(url="/contribute-asset/", permanent=False),
+        ),
+        url(r"^contribute$", RedirectView.as_view(url="/contribute/", permanent=False)),
+        url(
+            r"^contribute-activity$",
+            RedirectView.as_view(url="/contribute-activity/", permanent=False),
+        ),
+        url(
+            r"^contribute-asset$",
+            RedirectView.as_view(url="/contribute-asset/", permanent=False),
+        ),
+        # 2022-04-05 -- replaced contribute links with redirects
+        # url(r'^contribute/$', views.contribute),
+        # url(r'^contribute-activity/$', views.contribute_activity),
+        # url(r'^contribute-asset/$', views.contribute_asset),
+        url(r"^contribute/$", RedirectView.as_view(url="/", permanent=False)),
+        url(r"^contribute-activity/$", RedirectView.as_view(url="/", permanent=False)),
+        url(r"^contribute-asset/$", RedirectView.as_view(url="/", permanent=False)),
+        path("edit/<uuid:identifier>/", views.edit_resource),
+        # OLD: url(r'^edit/$', views.edit_resource),
+        # url(r'^page/materials/$', views.page__materials),
+        url(r"^thanks/$", views.thanks),
+        url(r"^schedule$", RedirectView.as_view(url="/events/", permanent=False)),
+        url(r"^schedule/$", RedirectView.as_view(url="/events/", permanent=False)),
+        url(r"^events$", RedirectView.as_view(url="/events/", permanent=False)),
+        url(r"^events/$", views.show_events),
+        url(r"^resources$", RedirectView.as_view(url="/resources/", permanent=False)),
+        url(r"^resources/$", views.show_resources),
+        path("events/<int:year>/<str:slug>/", views.show_event_detail),
+        path("resources/<int:year>/<str:slug>/", views.show_resource_detail),
+        # url(r'^page/home/$', views.index, name='web_index'),
+        path("cms/", include(wagtailadmin_urls)),
+        path("documents/", include(wagtaildocs_urls)),
+        path("about/", include(wagtail_urls)),
+        # path("search/", search_views.search, name="search"),
+    ]
+    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+)
