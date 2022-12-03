a34eef43598dc9d654f7c90664e65d6ba08ef208
Peter Hanecak <hanecak@hany.sk>
2022-12-03 17:55:45 +0100

    reformatting by black

--- web/data.py
+++ web/data.py
@@ -2,15 +2,15 @@ import geonamescache
 
 GC = geonamescache.GeonamesCache()
 
-COUNTRY_CHOICES = [(None, 'Please select an option below')]
+COUNTRY_CHOICES = [(None, "Please select an option below")]
 COUNTRY_CHOICES += [((c, c)) for c in sorted(GC.get_countries_by_names().keys())]
-COUNTRY_CHOICES += [('Country Not Listed', 'Country Not Listed')]
+COUNTRY_CHOICES += [("Country Not Listed", "Country Not Listed")]
 
 # --- --- --- --- ---
 
 # from https://github.com/ocwc/oerweek-frontend/blob/f62653f6b4f125271e0b2787c065b08a3f0d0a10/app/utils/get-active-languages.js
 
-LANGUAGE_LIST="""
+LANGUAGE_LIST = """
     Arabic
     Burmese
     Croatian
@@ -33,7 +33,9 @@ LANGUAGE_LIST="""
     Other
 """
 
-LANGUAGE_CHOICES = [(None, 'Please select an option below')]
-LANGUAGE_CHOICES += [((c.strip(), c.strip())) for c in LANGUAGE_LIST.strip().split('\n')]
+LANGUAGE_CHOICES = [(None, "Please select an option below")]
+LANGUAGE_CHOICES += [
+    ((c.strip(), c.strip())) for c in LANGUAGE_LIST.strip().split("\n")
+]
 
 # --- --- --- --- ---
