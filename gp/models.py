54baa0fe5c3cbae68179f870c294b14f7214fab3
Peter Hanecak <hanecak@hany.sk>
2022-12-16 17:48:50 +0100

    first version of geenric page

--- gp/models.py
+++ gp/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+from wagtail.admin.panels import FieldPanel
+from wagtail.fields import StreamField
+from wagtail.models import Page
+
+from .blocks import BaseStreamBlock
+
+
+class GenericPage(Page):
+    """
+    A generic content page for OE Week.
+    """
+
+    body = StreamField(
+        BaseStreamBlock(),
+        verbose_name="Generic page content block",
+        blank=True,
+        use_json_field=True,
+    )
+
+    content_panels = Page.content_panels + [
+        FieldPanel("body"),
+    ]
+
+    def __str__(self):
+        return "generic page: %s" % self.title
