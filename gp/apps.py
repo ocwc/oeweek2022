fd46cea2a4a7c25751d73a20f9919ff60e821ea5
Peter Hanecak <hanecak@hany.sk>
2022-12-16 16:30:53 +0100

    reformatting by black

--- gp/apps.py
+++ gp/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class GpConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'gp'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "gp"
