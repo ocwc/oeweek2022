2d5cf96aea4455930a500662f09a9ab962d51d79
Jan Gondol <jan@jangondol.com>
2023-01-03 21:06:09 +0100

    Fix crash during initial setup

--- web/apps.py
+++ web/apps.py
@@ -10,11 +10,15 @@ class WebConfig(AppConfig):
     def ready(self):
         from django_q.tasks import schedule, Schedule
 
-        if Schedule.objects.filter(name=self.CLEANUP_TASK_NAME_V1).exists():
-            return
+        # FYI: try-except exists to avoid "sqlite3.OperationalError: no such table: django_q_schedule" during initial setup
+        try:
+            if Schedule.objects.filter(name=self.CLEANUP_TASK_NAME_V1).exists():
+                return
 
-        schedule(
-            "web.magiclink_utils.delete_disabled_magic_links",
-            name=self.CLEANUP_TASK_NAME_V1,
-            schedule_type=Schedule.DAILY,
-        )
+            schedule(
+                "web.magiclink_utils.delete_disabled_magic_links",
+                name=self.CLEANUP_TASK_NAME_V1,
+                schedule_type=Schedule.DAILY,
+            )
+        except:
+            pass
