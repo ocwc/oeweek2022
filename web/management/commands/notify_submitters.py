caaf8fca6e5eba2b3eef1f79309297bb1f63fd9e
Peter Hanecak <hanecak@hany.sk>
2023-01-14 17:59:20 +0100

    reformatting by black

--- web/management/commands/notify_submitters.py
+++ web/management/commands/notify_submitters.py
@@ -14,42 +14,44 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
 
         for resource in Resource.objects.filter(
-                year=2022,
-                # created__year=2022,
-                post_status='publish',
-                notified=False,
-                # email='example@example.org',
-        ):#.exclude(email='')[:10]:
+            year=2022,
+            # created__year=2022,
+            post_status="publish",
+            notified=False,
+            # email='example@example.org',
+        ):  # .exclude(email='')[:10]:
 
             # an extra check, just in case...
-            if resource.notified==True:
-                print(f'Skipping {resource.email} about #{resource.id} (already notified)')
+            if resource.notified == True:
+                print(
+                    f"Skipping {resource.email} about #{resource.id} (already notified)"
+                )
                 continue
 
-            if resource.post_type == 'event':
-                slug1 = 'events'
-            elif resource.post_type == 'resource':
-                slug1 = 'resources'
+            if resource.post_type == "event":
+                slug1 = "events"
+            elif resource.post_type == "resource":
+                slug1 = "resources"
 
             context = {
-                'slug1': slug1,
-                'year': resource.year,
-                'slug2': resource.slug,
-                'title': resource.title,
-                'firstname': resource.firstname,
+                "slug1": slug1,
+                "year": resource.year,
+                "slug2": resource.slug,
+                "title": resource.title,
+                "firstname": resource.firstname,
             }
 
             try:
                 send_mail(
                     "emails/accepted.tpl",
-                    context, # {}
+                    context,  # {}
                     "info@openeducationweek.org",
                     [resource.email],
-                    cc=['openeducationweek@oeglobal.org'],
+                    cc=["openeducationweek@oeglobal.org"],
                 )
                 resource.notified = True
                 resource.save()
-                print(f'Emailed {resource.email} about #{resource.id}')
+                print(f"Emailed {resource.email} about #{resource.id}")
                 time.sleep(2)
             except:
-                print(f'Failed to email {resource.email} about #{resource.id}')
+                print(f"Failed to email {resource.email} about #{resource.id}")
